0ba2b2d6247de82af92b587fbec52bec53c8d19a
Luke Ferris <luke.ferris@gmail.com>
2023-10-28 17:00:56 +0200

    Added map additional props

--- map.jsx
+++ map.jsx
@@ -10,6 +10,8 @@ import MapGL from "@ferris440/origin-library/src/components/map/mapGL";
 // END DO NOT REMOVE THIS TEXT
 
 // A MAP GL instance, takes a theme and any other valid MAPGL prop e.g. initialViewState
-export default function Map() {
-  return <MapGL mapStyle="mapbox://styles/mapbox/streets-v11" />;
+export default function Map({ otherMapGLPops }) {
+  return (
+    <MapGL mapStyle="mapbox://styles/mapbox/streets-v11" {...otherMapGLPops} />
+  );
 }
