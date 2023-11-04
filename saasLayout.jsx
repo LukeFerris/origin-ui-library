f47489b7ff0d6f20865e07dd5349b7ea4505c331
Luke Ferris <luke.ferris@gmail.com>
2023-11-04 11:34:58 +0100

    Added saasLayout

---
+++ saasLayout.jsx
@@ -0,0 +1,31 @@
+import { Box, Toolbar } from "@mui/material";
+import { Routes, Route } from "react-router-dom";
+const drawerWidth = 240;
+
+export default function SaaSLayoutContainer() {
+  return (
+    <Box sx={{ display: "flex", height: "100%" }}>
+      [MENU COMPONENT GOES HERE IF REQUIRED]
+      <Box
+        component="main"
+        sx={{
+          SideNavigationBarGrow: 1,
+          display: "flex",
+          flexDirection: "column",
+          p: 3,
+          width: { sm: `calc(100% - ${drawerWidth}px)` },
+          padding: "0px",
+        }}
+      >
+        <Toolbar />
+        <div style={{ padding: "20px", flexGrow: 1, overflow: "hidden" }}>
+          <Routes>
+            {/* example content routes */}
+            {/* <Route path="/" element={<HomePage />} /> */}
+            {/* <Route path="/about" element={<AboutPage />} /> */}
+          </Routes>
+        </div>
+      </Box>
+    </Box>
+  );
+}
