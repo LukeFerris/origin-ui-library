60807dafc8544e9fd084adee6459d3a8a5d9b345
Luke Ferris <luke.ferris@gmail.com>
2023-11-07 12:08:51 +0100

    Added muiIcon

---
+++ muiIcon.jsx
@@ -0,0 +1,8 @@
+// make sure you change the icon to be one of the valid
+// MUI material ui icons
+import HomeIcon from "@mui/icons-material/Home";
+
+export default function IconName() {
+  // just return the icon
+  return <Home />;
+}
