4796a95bccf7b10378d41d61be3da6b4e8123146
Luke Ferris <luke.ferris@gmail.com>
2023-11-07 17:30:56 +0100

    Updated the guidance for simpleLayout

--- simpleLayout.jsx
+++ simpleLayout.jsx
@@ -1,8 +1,10 @@
 import "./App.css";
 import HoldingPage_Component from "./HoldingPage_Component";
 
-// This page uses react-router V6
+// IMPORTANT DO NOT CHANGE
+// This page uses react-router V6 which does not have Switch
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+// END IMPORTANT
 
 // This page is currently used to provide structure and layout to the overall site
 export default function MainPage() {
